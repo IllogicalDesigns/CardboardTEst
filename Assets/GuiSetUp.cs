0aec6a3ec2ae5ba483dfce9a4b6b70fc32c3c2b9
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-14 21:26:39 -0800

    super fun stuff day 3
    yep more art some code still no movement

---
+++ Assets/GuiSetUp.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiSetUp : MonoBehaviour
+{
+		public Canvas masterCanvas;
+		public Camera leftEye;
+		public Camera rightEye;
+
+		// Use this for initialization
+		void Start ()
+		{
+				Canvas leftCanvas;
+				leftCanvas = Instantiate (masterCanvas, transform.position, Quaternion.identity) as Canvas;
+				Canvas rightCanvas;
+				rightCanvas = Instantiate (masterCanvas, transform.position, Quaternion.identity) as Canvas;
+				//set pixel perfect is it is
+				if (masterCanvas.pixelPerfect == true) {
+						leftCanvas.pixelPerfect = true;
+						rightCanvas.pixelPerfect = true;
+				} else {
+						leftCanvas.pixelPerfect = false;
+						rightCanvas.pixelPerfect = false;
+				}
+				//set plane distance to that of master Canvas
+				float planeDis = masterCanvas.planeDistance;
+				leftCanvas.planeDistance = planeDis;
+				rightCanvas.planeDistance = planeDis;
+				//set our redering mode
+				leftCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+				rightCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+				//set our canvases to the right eyes
+				leftCanvas.worldCamera = leftEye;
+				rightCanvas.worldCamera = rightEye;
+				//set our Master to not render
+				masterCanvas.gameObject.SetActive (false);
+		}
+}
---
+++ Assets/RotateAndMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAndMove : MonoBehaviour
+{
+		// Update is called once per frame
+		void Update ()
+		{
+		float rotX = (Cardboard.SDK.HeadRotation.x - Cardboard.SDK.HeadRotation.x / 2);
+		Quaternion rot = new Quaternion (rotX, Cardboard.SDK.HeadRotation.y, Cardboard.SDK.HeadRotation.z, Cardboard.SDK.HeadRotation.w);
+						transform.rotation = rot;
+		}
+}
