5c96805834b3764a435af6da8e39472d5553b25e
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-16 00:30:05 -0800

    Laptop 2 big Comp day 4
    got movement/rotation working I think.  Possible fix for locAL multi jam
    just worng kind of movement code chekc new in this app.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+		public GameObject playerHead;
+		public int speed = 5;
+		public float headHeight = 1.5f;
+		private float h;
+		private float v;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				h = Input.GetAxis ("Horizontal");
+				v = Input.GetAxis ("Vertical");
+		}
+
+		void FixedUpdate ()
+		{
+				//Sets our rotation to  our head
+				//transform.rotation = new Quaternion (transform.rotation.x, playerHead.transform.position.y, transform.rotation.z, playerHead.transform.rotation.w);
+		Quaternion rot = new Quaternion (transform.rotation.x, Cardboard.SDK.HeadRotation.y, transform.rotation.z, Cardboard.SDK.HeadRotation.w);
+				transform.rotation = rot;
+
+				playerHead.transform.position = new Vector3 (transform.position.x, transform.position.y + headHeight, transform.position.z); 
+				
+				//mkes to places to move towards
+				Vector3 hVec3 = new Vector3 (transform.position.x + 1f, transform.position.y, transform.position.z);
+				Vector3 vVec3 = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1f);
+				
+				//does the move torwards
+				float step = speed * Time.deltaTime * h;
+				transform.position = Vector3.MoveTowards (transform.position, hVec3, step);
+				float step2 = speed * Time.deltaTime * v;
+				transform.position = Vector3.MoveTowards (transform.position, vVec3, step2);
+		}
+}
