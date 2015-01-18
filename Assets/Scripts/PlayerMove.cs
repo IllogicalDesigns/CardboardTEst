36e77e7268142b083c9bc2ff884e417ed05ffabc
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-17 21:17:39 -0800

    staying alive ha ha ha ha staing alive
    im dead now

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerMove : MonoBehaviour
 {
 		public GameObject playerHead;
+		public Transform tarV;
+		public Transform tarH;
 		public int speed = 5;
 		public float headHeight = 1.5f;
 		private float h;
@@ -26,19 +28,19 @@ public class PlayerMove : MonoBehaviour
 		{
 				//Sets our rotation to  our head
 				//transform.rotation = new Quaternion (transform.rotation.x, playerHead.transform.position.y, transform.rotation.z, playerHead.transform.rotation.w);
-		Quaternion rot = new Quaternion (transform.rotation.x, Cardboard.SDK.HeadRotation.y, transform.rotation.z, Cardboard.SDK.HeadRotation.w);
+				Quaternion rot = new Quaternion (transform.rotation.x, Cardboard.SDK.HeadRotation.y, transform.rotation.z, Cardboard.SDK.HeadRotation.w);
 				transform.rotation = rot;
 
 				playerHead.transform.position = new Vector3 (transform.position.x, transform.position.y + headHeight, transform.position.z); 
 				
 				//mkes to places to move towards
-				Vector3 hVec3 = new Vector3 (transform.position.x + 1f, transform.position.y, transform.position.z);
-				Vector3 vVec3 = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1f);
+				//Vector3 hVec3 = new Vector3 (transform.position.x + 1f, transform.position.y, transform.position.z);
+				//Vector3 vVec3 = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1f);
 				
 				//does the move torwards
 				float step = speed * Time.deltaTime * h;
-				transform.position = Vector3.MoveTowards (transform.position, hVec3, step);
+				transform.position = Vector3.MoveTowards (transform.position, tarH.position, step);
 				float step2 = speed * Time.deltaTime * v;
-				transform.position = Vector3.MoveTowards (transform.position, vVec3, step2);
+				transform.position = Vector3.MoveTowards (transform.position, tarV.position, step2);
 		}
 }
