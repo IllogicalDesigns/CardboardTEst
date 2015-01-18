7229fc33dd2e3abbfb3d54772a387af31173e99d
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-18 01:44:27 -0800

    Dying now im dying now Day 5
    moving the first of the racing test over

---
+++ Assets/Scripts/Racing/carMove.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class carMove : MonoBehaviour
+{
+		public GameObject frontAxel;
+		public GameObject backAxel;
+		public bool frontDrive = true;
+		public float acceleration = 5f;
+		public float maxSpeed = 150f;
+		public float currentSpeed;
+		public Vector3 yourLocalForcePosition;
+		public List<GameObject> wheels = new List<GameObject> ();
+		GameObject myParent;
+		float h;
+		float v;
+
+		// Use this for initialization
+		void Start ()
+		{
+				myParent = gameObject.transform.parent.gameObject;
+				List<GameObject> Axels = new List<GameObject> ();
+				foreach (Transform child in myParent.transform) {
+						if (child.gameObject.tag == "Axel") {
+								foreach (Transform dChild in child.transform) {
+										if (dChild.gameObject.tag == "Wheel") {
+												wheels.Add (dChild.gameObject);
+										}
+								}
+						}
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				h = Input.GetAxis ("Horizontal");
+				v = Input.GetAxis ("Vertical");
+				Debug.Log (rigidbody.velocity.magnitude);
+		}
+
+		void FixedUpdate ()
+		{
+				if (rigidbody.velocity.sqrMagnitude > maxSpeed * maxSpeed) {
+						rigidbody.velocity = Vector3.ClampMagnitude (rigidbody.velocity, maxSpeed);
+				}
+				if (v != 0 && !(rigidbody.velocity.magnitude > maxSpeed)) {
+						Vector3 worldForcePosition = transform.TransformPoint (yourLocalForcePosition);
+						rigidbody.AddForceAtPosition (transform.forward * (acceleration * v), worldForcePosition);
+				}
+		}
+}
