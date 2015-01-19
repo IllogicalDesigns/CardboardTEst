a7a9d93118924d46615ef98e2499128433fcd763
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-18 21:55:11 -0800

    Undead Now im killing things im killing things Day 6

--- Assets/Scripts/Racing/carMove.cs
+++ Assets/Scripts/Racing/carMove.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 
 public class carMove : MonoBehaviour
 {
-		public GameObject frontAxel;
-		public GameObject backAxel;
 		public bool frontDrive = true;
 		public float acceleration = 5f;
 		public float maxSpeed = 150f;
 		public float currentSpeed;
 		public Vector3 yourLocalForcePosition;
 		public List<GameObject> wheels = new List<GameObject> ();
+		public Rigidbody backAxel;
+		public GameObject RFWheel;
+		public GameObject LFWheel;
 		GameObject myParent;
 		float h;
 		float v;
@@ -19,13 +20,21 @@ public class carMove : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-				myParent = gameObject.transform.parent.gameObject;
-				List<GameObject> Axels = new List<GameObject> ();
-				foreach (Transform child in myParent.transform) {
-						if (child.gameObject.tag == "Axel") {
-								foreach (Transform dChild in child.transform) {
-										if (dChild.gameObject.tag == "Wheel") {
-												wheels.Add (dChild.gameObject);
+				myParent = gameObject.transform.parent.gameObject; 																				//Find my parent
+				foreach (Transform child in myParent.transform) {																				//get my parents children
+						if (child.gameObject.tag == "Axel") {																					//if they are an axel
+								if (child.gameObject.name.ToString ().Contains ("Back") || child.gameObject.name.ToString ().Contains ("back"))	//our they the back axel must be called back something
+										backAxel = child.gameObject.rigidbody;																	//add back axel reference
+								foreach (Transform dChild in child.transform) {																	//find the axels children
+										if (dChild.gameObject.tag == "Wheel") {																	//our these the wheels we are looking for
+												wheels.Add (dChild.gameObject);																	//make reference to them in wheels
+												if (dChild.gameObject.name.ToString ().Contains ("f") || dChild.gameObject.name.ToString ().Contains ("F")) {	//our we a front wheel
+														if (dChild.gameObject.name.Contains ("r") || dChild.gameObject.name.ToString ().Contains ("R")) {		//our we the right wheel
+																RFWheel = dChild.gameObject;													//then we our the front right wheel
+														} else {																				//else
+																LFWheel = dChild.gameObject;													//then we our the front left wheel
+														}
+												}
 										}
 								}
 						}
@@ -37,7 +46,6 @@ public class carMove : MonoBehaviour
 		{
 				h = Input.GetAxis ("Horizontal");
 				v = Input.GetAxis ("Vertical");
-				Debug.Log (rigidbody.velocity.magnitude);
 		}
 
 		void FixedUpdate ()
