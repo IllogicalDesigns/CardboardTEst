11e5dd4b6ded55abb99b04ec865aeb56269e951e
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-19 17:57:46 -0800

    vroom vroom partial new car script
    my car cript did not work so I found one and I am adapting it now

---
+++ Assets/Scripts/Racing/PlayerCar.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCar : MonoBehaviour {
+	private Transform carTransform;										//our transform
+	private Rigidbody carRidgidbody;									//our ridgidbody
+	private Vector3 carUp;												//our up			
+	private Vector3 carRight;											//our right
+	private Vector3 carForward;											//our forward
+	private Vector3 carBackwards;										//our back	
+
+	public Vector3 centerOfGravity = new Vector3(0,-0.7,0.35);			//how far do we force the cog down 
+	public float carMass;												//how heavy is our car
+
+	private float h;													//our Horizantal axis position used for steering
+	private float v;													//our Vertical axis Position used for throttle and brake
+
+	void initilizer () 
+	{
+		//cache this car's transform
+		carTransform = transform;
+		//cache this car's ridgidbody
+		carRidgidbody = rigidbody;
+		//set up this car's mass
+		carMass = carRidgidbody.mass;
+		//to prevent the car from fliping we need to set the car's center of gravity lower
+		if (centerOfGravity != Vector3.zero)
+						//set it to our custom if we have one
+						carRidgidbody.centerOfMass = centerOfGravity;
+				else
+						//set it to unity's value if we don't
+						centerOfGravity = carRidgidbody.centerOfMass;
+		//cache this car's vectors
+		carUp = carTransform.up;
+		carUp = carTransform.right;
+		carUp = carTransform.forward;
+		//start the set up wheels function
+		setUpWheels ();
+	}
+
+	void setUpWheels()
+	{
+
+	}
+
+	void carPhysicsUpdate ()
+	{
+
+	}
+
+	void rotateVisualWheels ()
+	{
+
+	}
+	void updateEngineSound ()
+	{
+
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		initilizer ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		carPhysicsUpdate ();
+	}
+	void LateUpdate () 
+	{
+		//turn the visual wheels
+		rotateVisualWheels ();
+		//make the vroom! vrooom!
+		updateEngineSound ();
+	}
+}
--- Assets/Scripts/Racing/carMove.cs
+++ Assets/Scripts/Racing/carMove.cs
@@ -8,11 +8,13 @@ public class carMove : MonoBehaviour
 		public float acceleration = 5f;
 		public float maxSpeed = 150f;
 		public float currentSpeed;
-		public Vector3 yourLocalForcePosition;
-		public List<GameObject> wheels = new List<GameObject> ();
-		public Rigidbody backAxel;
+		public GameObject backAxel;
 		public GameObject RFWheel;
 		public GameObject LFWheel;
+		public GameObject RBWheel;
+		public GameObject LBWheel;
+		public Transform forceLocation;
+		public Vector3 test;
 		GameObject myParent;
 		float h;
 		float v;
@@ -20,11 +22,12 @@ public class carMove : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
+				List<GameObject> wheels = new List<GameObject> ();
 				myParent = gameObject.transform.parent.gameObject; 																				//Find my parent
 				foreach (Transform child in myParent.transform) {																				//get my parents children
 						if (child.gameObject.tag == "Axel") {																					//if they are an axel
 								if (child.gameObject.name.ToString ().Contains ("Back") || child.gameObject.name.ToString ().Contains ("back"))	//our they the back axel must be called back something
-										backAxel = child.gameObject.rigidbody;																	//add back axel reference
+										backAxel = child.gameObject;																			//add back axel reference
 								foreach (Transform dChild in child.transform) {																	//find the axels children
 										if (dChild.gameObject.tag == "Wheel") {																	//our these the wheels we are looking for
 												wheels.Add (dChild.gameObject);																	//make reference to them in wheels
@@ -33,6 +36,12 @@ public class carMove : MonoBehaviour
 																RFWheel = dChild.gameObject;													//then we our the front right wheel
 														} else {																				//else
 																LFWheel = dChild.gameObject;													//then we our the front left wheel
+														}
+												} else {
+														if (dChild.gameObject.name.Contains ("r") || dChild.gameObject.name.ToString ().Contains ("R")) {//our we the right wheel
+																RBWheel = dChild.gameObject;													//then we our the front right wheel
+														} else {																				//else
+																LBWheel = dChild.gameObject;													//then we our the front left wheel
 														}
 												}
 										}
@@ -51,11 +60,11 @@ public class carMove : MonoBehaviour
 		void FixedUpdate ()
 		{
 				if (rigidbody.velocity.sqrMagnitude > maxSpeed * maxSpeed) {
-						rigidbody.velocity = Vector3.ClampMagnitude (rigidbody.velocity, maxSpeed);
+					
 				}
-				if (v != 0 && !(rigidbody.velocity.magnitude > maxSpeed)) {
-						Vector3 worldForcePosition = transform.TransformPoint (yourLocalForcePosition);
-						rigidbody.AddForceAtPosition (transform.forward * (acceleration * v), worldForcePosition);
+				if (v != 0) {
+						Vector3 worldForcePosition = transform.TransformPoint (test);
+						rigidbody.AddForceAtPosition (transform.forward * (acceleration * v), worldForcePosition, ForceMode.Acceleration);
 				}
 		}
 }
