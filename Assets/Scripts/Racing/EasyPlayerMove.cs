575f6894cedd228eae02421b758f87a8820ec730
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-01-27 21:29:32 -0800

    carModel 2Laptop

--- Assets/Scripts/Racing/EasyPlayerMove.cs
+++ Assets/Scripts/Racing/EasyPlayerMove.cs
@@ -11,7 +11,6 @@ public class EasyPlayerMove : MonoBehaviour
 		public float topSpeed = 10f;
 		public float downPressureFactor = 0.5f;
 		public float massCenterY = -0.1f;
-		public Transform myEngineForceLocation;
 		public WheelCollider[] myColliderWheels = new WheelCollider[4]; 				//0LF 1LB 2RF 3RB
 		public Transform[] myVisualWheels = new Transform[4]; 							//0LF 1LB 2RF 3RB
 		public WheelCollider[] myTurnColl = new WheelCollider[2];						//0 == left && 1 == right
@@ -37,10 +36,9 @@ public class EasyPlayerMove : MonoBehaviour
 						rigidbody.AddForce (downPressure, ForceMode.Acceleration);
 				}
 		}
-
-	Vector3 GetWheelPos (WheelCollider myWheelColl, Transform myWheel)
+		Vector3 GetWheelPos (WheelCollider myWheelColl, Transform myWheel)
 		{
-		RaycastHit hit;
+				RaycastHit hit;
 				if (Physics.Raycast (myWheelColl.transform.position, -myWheelColl.transform.up, out hit, myWheelColl.suspensionDistance + myWheelColl.radius)) {
 						myWheel.position = hit.point + myWheelColl.transform.up * myWheelColl.radius; 
 				} else {
@@ -48,19 +46,18 @@ public class EasyPlayerMove : MonoBehaviour
 				}
 				return myWheel.position;
 		}
-
 		void UpdateVisualWheels ()
 		{
 				Quaternion newRotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-				newRotation *= Quaternion.Euler (0f, 0f, mySteer * h); // this add a 90 degrees Y rotation
-				for (int i = 0; i < myTurnWheels.Length; i++) {
-						myTurnWheels [i].transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, 20f * Time.deltaTime);
-				}
+				newRotation *= Quaternion.Euler (0f, mySteer * h, 0f); // this add a 90 degrees Y rotation
 				for (int i = 0; i < myTurnColl.Length; i++) {
 						myTurnColl [i].steerAngle = (mySteer / 2f) * h;
 				}
 				for (int i = 0; i < myVisualWheels.Length; i++) {
 					myVisualWheels[i].transform.position = GetWheelPos(myColliderWheels[i], myVisualWheels[i]) ;
+				}
+				for (int i = 0; i < myTurnWheels.Length; i++) {
+					myTurnWheels [i].transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, 20f * Time.deltaTime);
 				}
 				
 		}
