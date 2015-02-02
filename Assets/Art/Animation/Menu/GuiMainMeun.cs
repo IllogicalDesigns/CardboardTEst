c2aa7440582769a86ef2665e8645e42f9f515b8c
tinykidtoo <tinykidtoodotcom@yahoo.com>
2015-02-02 11:46:32 -0800

    2big comp day 17

--- Assets/Art/Animation/Menu/GuiMainMeun.cs
+++ Assets/Art/Animation/Menu/GuiMainMeun.cs
@@ -4,22 +4,81 @@ using System.Collections;
 
 public class GuiMainMeun : MonoBehaviour
 {
-	public string lvl2Load = "FIX_ME";
+		public string lvl2Load = "FIX_ME";
 		public Animator[] myMainMenuAnimators;
-
+		public Canvas[] myMainCanvas;				//Main = 1 //Options = 2 //RaceSetup = 3 //Extras = 4
+		public Text[] myOptionsText;
+		public Slider[] mySliders;
+		public static int mySensitivity = 5;
+		public static float myVolume = 0.5f;
+		public static int myGraphics = 5;
+		public string[] names;
 		// Use this for initialization
 		void Start ()
 		{
-	
+				QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("qualityLevel"));
+		mySliders[2].value = PlayerPrefs.GetInt("qualityLevel");
+				foreach (Canvas tempCan in myMainCanvas) {
+						tempCan.enabled = false;
+				}
+				myMainCanvas [0].enabled = true;
+				names = QualitySettings.names;
+				mySliders [2].maxValue = (names.Length - 1);
 		}
+
 		public void LoadLevel ()
-	{
-		Application.LoadLevel (lvl2Load);
-		Debug.Log (lvl2Load);
-	}
+		{
+				Application.LoadLevel (lvl2Load);
+				Debug.Log (lvl2Load);
+		}
+
 		public void QuitGame ()
 		{
-		Application.Quit ();
+				Application.Quit ();
+		}
+
+		public void SwitchToOptions ()
+		{
+				foreach (Canvas tempCan in myMainCanvas) {
+						tempCan.enabled = false;
+				}
+				myMainCanvas [1].enabled = true;
+		}
+
+		public void SwitchToMainMeun ()
+		{
+				foreach (Canvas tempCan in myMainCanvas) {
+						tempCan.enabled = false;
+				}
+				myMainCanvas [0].enabled = true;
+		}
+
+		public void OptionsUpdateSensetivity ()
+		{
+				int tmpInt = Mathf.RoundToInt (mySliders [0].value);
+				myOptionsText [0].text = (myOptionsText [0].name + " : " + Mathf.RoundToInt (mySliders [0].value).ToString ());
+				mySensitivity = tmpInt;
+		}
+
+		public void OptionsUpdateVolume ()
+		{
+				int tmpInt = Mathf.RoundToInt (mySliders [1].value);
+				myOptionsText [1].text = (myOptionsText [1].name + " : " + Mathf.RoundToInt (mySliders [1].value).ToString ());
+				//myVolume = (Mathf.RoundToInt (mySliders [1].value) );
+		}
+
+		public void OptionsUpdateGraphics ()
+		{
+				int tmpInt = Mathf.RoundToInt (mySliders [2].value);
+				if (tmpInt < 0)
+						tmpInt = 0;
+
+				myOptionsText [2].text = (myOptionsText [2].name + " : " + names [tmpInt]);
+		}
+		public void SaveAndReturnOptions ()
+		{
+		QualitySettings.SetQualityLevel (Mathf.RoundToInt(mySliders [2].value));
+		PlayerPrefs.SetInt ("qualityLevel", Mathf.RoundToInt(mySliders [2].value));
 		}
 
 		public void CloseMainMenu ()
